52edf889755ce126acd6699e3253556cb923950f
Yulia Gapunovych <45093751+YulitaGap@users.noreply.github.com>
2020-09-18 01:19:58 +0300

    Update shell_make.sh

--- shell_make.sh
+++ shell_make.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ## AUTHOR: Yulia Gapunovych
 
@@ -6,7 +6,7 @@
 ## exit the shell(default status code: 1) after printing the message to stderr
 bail() {
     echo -ne "$1" >&2
-    exit ${2-1}
+    exit "${2-1}"
 } 
 
 ## help message
@@ -24,7 +24,7 @@ usage() {
         status=$1
         shift
     fi
-    bail "${1}$HELP_MSG" $status
+    bail "${1}$HELP_MSG" "$status"
 }
 
 while getopts ":hf:" opt; do
@@ -58,12 +58,55 @@ shift $((OPTIND -1))
 #==========MAIN CODE BELOW==========
 
 # reading makefile
-
 if ! test -f "$makefile"; then
 echo "make: $makefile : No such file or directory " && bail
 fi 
 
-while IFS= read -r line
-do
-  
-done < "$makefile"
+declare -A target_prerequisites
+declare -A target_commands
+
+IFS=$'\n'
+while read -r line; do
+    if [[ $line == *:* ]]; then
+
+    #splitting by ':'
+        IFS=':' read -r -a parts <<<"$line"
+        curr_target="${parts[0]}"
+        curr_prerequisites="${parts[1]}"
+
+    #check if already exists built target
+        if  test -f "$curr_target"; then
+        echo "make: $curr_target is up to date. " && bail
+        fi 
+    
+    
+    #splitting by ' '
+        target_prerequisites["$curr_target"]=""
+        if ! [ -z "$curr_prerequisites" ];
+        then
+        IFS=' ' read -ra prerequisites <<< "$curr_prerequisites"
+            for i in "${prerequisites[@]}"
+            do
+                target_prerequisites["$curr_target"]+="$i"
+            done
+        fi 
+
+    #getting commands for target
+    elif ! [ -z "$line" ];then
+        COMMAND="${line/$(printf '\t')/}"
+        target_commands["$curr_target"]="$COMMAND"
+    fi
+done <"$makefile"
+
+# building targets
+for x in "${!target_prerequisites[@]}"; do
+    dep="${target_prerequisites[$x]//[$'\t\r\n ']/}"
+    com="${target_commands[$x]}"
+    
+    if ! [ -z "$dep" ];then 
+        make "$dep"
+    fi
+    
+    echo "$com"
+    eval "${target_commands[$x]}"
+done
